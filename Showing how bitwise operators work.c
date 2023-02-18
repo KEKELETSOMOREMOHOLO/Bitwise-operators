4fe0a922b39014982371d62e9089cdfa223849c7
KEKELETSOMOREMOHOLO <118248938+KEKELETSOMOREMOHOLO@users.noreply.github.com>
2023-02-18 12:11:52 +0530

    Create Showing how bitwise operators work.c

---
+++ Showing how bitwise operators work.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 12, b = 25;
+    printf("Output = %d", a & b);
+    printf("Output = %d", a | b);
+    printf("Output = %d", a ^ b);
+
+    return 0;
+}
